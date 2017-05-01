059d43f7af971eb9a5c63d8772768188750eb02c
NatalyCreates <nataly@natalycreates.com>
2017-05-02 00:57:42 +0300

    working clickable cube

---
+++ Assets/Scripts/HandleClicks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HandleClicks : MonoBehaviour, IPointerClickHandler {
+
+    void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
+    {
+        Destroy(gameObject);
+    }
+	
+}
